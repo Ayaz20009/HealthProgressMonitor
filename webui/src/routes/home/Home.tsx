e561affa664374f6bdb693c53568aee6c45c3735
Ayaz Shah <ayazshah_20009@yahoo.com>
2024-01-13 22:24:52 -0500

    project setup

--- webui/src/routes/home/Home.tsx
+++ webui/src/routes/home/Home.tsx
@@ -2,8 +2,35 @@ import "../home/Home.css";
 import { Col, Row } from "react-bootstrap";
 import { H1 } from "@leafygreen-ui/typography";
 import Layout from "../../components/Layout/Layout";
+import { useState, useEffect } from "react";
+import { app } from "../../realmApp/realmApp";
+import FormFooter from "@leafygreen-ui/form-footer";
+import * as Realm from "realm-web";
 
 export const HomeComponent = () => {
+
+  const [user, setUser] = useState<any>();
+
+  useEffect(() => {
+    //Runs only on the first render
+    loginApiKey(process.env.REACT_APP_REALM_API_KEY); // add generated API key in .env file
+  }, []);
+
+  const loginApiKey = async (apiKey: any) => {
+    // use an API Key credential to login to Realm
+    const credentials = Realm.Credentials.apiKey(apiKey);
+
+    // Authenticate the user
+    setUser(await app.logIn(credentials))
+  }
+
+  const testing = async () => {
+    if (user === undefined) return;
+
+    const result = await user.functions.testFunction();
+    console.log(result);
+  };
+
   return (
     <Layout>
       <div>
@@ -14,7 +41,16 @@ export const HomeComponent = () => {
             </div>
           </Col>
         </Row>
-
+        <Row>
+          <Col>
+            <FormFooter
+              onClick={testing}
+              primaryButton={{
+                text: 'Submit',
+              }}
+            />
+          </Col>
+        </Row>
       </div>
     </Layout>
   );
